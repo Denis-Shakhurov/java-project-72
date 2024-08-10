ee189c7337e6ba9f1d1eee81e1dbf3ccb419913c
DenisShakhurov <disa.sdf86@gmail.com>
2024-08-10 13:00:52 +0700

    add new files

---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("java")
+    application
+    checkstyle
+    jacoco
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+jacoco {
+    toolVersion = "0.8.11"
+    reportsDirectory = layout.buildDirectory.dir("customJacocoReportDir")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
