a4ef26769b7757734072e84eb0f53e10cb1046fa
DenisShakhurov <disa.sdf86@gmail.com>
2024-08-11 08:38:18 +0700

    add new file

---
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -0,0 +1,18 @@
+package hexlet.code.model;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Url {
+    private Long id;
+    private String name;
+    private LocalDateTime createdAt;
+
+    public Url(String name, LocalDateTime createdAt) {
+        this.name = name;
+        this.createdAt = createdAt;
+    }
+}
