a705aaca7af7a21ffec3d0bfc411389fb5444380
DenisShakhurov <disa.sdf86@gmail.com>
2024-08-13 18:59:46 +0700

    add UrlChecksController.java

---
+++ app/src/main/java/hexlet/code/controller/UrlChecksController.java
@@ -0,0 +1,47 @@
+package hexlet.code.controller;
+
+import hexlet.code.model.UrlCheck;
+import hexlet.code.repository.UrlCheckRepository;
+import hexlet.code.repository.UrlRepository;
+import hexlet.code.util.NamedRoutes;
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
+import kong.unirest.core.Unirest;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+public class UrlChecksController {
+
+    public static void create(Context ctx) throws SQLException, IOException {
+        var urlId = ctx.pathParamAsClass("id", Long.class).get();
+        var url = UrlRepository.find(urlId)
+                .orElseThrow(() -> new NotFoundResponse("url не найден"));
+        var urlName = url.getName();
+        var jsonResponse = Unirest.get(urlName).asString();
+        var statusCode = jsonResponse.getStatus();
+        var title = findText(urlName,"title");
+        var h1 = findText(urlName,"h1");
+        var description = findText(urlName,"description");
+        var createdAt = LocalDateTime.now();
+        var urlCheck = new UrlCheck(statusCode, h1, title, description, urlId, createdAt);
+        UrlCheckRepository.save(urlCheck);
+        ctx.redirect(NamedRoutes.urlPath(urlId));
+    }
+
+    public static String findText(String url, String tag) throws IOException {
+        Document document = Jsoup.connect(url).userAgent("Chrome/81.0.4044.138").get();
+        switch (tag) {
+            case "title" : return document.title();
+            case "h1" : return document.selectFirst("h1") == null ? ""
+                : document.selectFirst("h1").text();
+            case "description" :
+                return document.selectFirst("meta[name=description]") == null ? ""
+                        : document.selectFirst("meta[name=description]").attr("content");
+            default:return "";
+        }
+    }
+}
