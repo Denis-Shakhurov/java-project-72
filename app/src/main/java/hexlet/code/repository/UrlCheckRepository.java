5b820b131dd4f7437dff159782d7be2059cb0d83
DenisShakhurov <disa.sdf86@gmail.com>
2024-08-13 19:01:30 +0700

    add UrlCheckRepository.java

---
+++ app/src/main/java/hexlet/code/repository/UrlCheckRepository.java
@@ -0,0 +1,55 @@
+package hexlet.code.repository;
+
+import hexlet.code.model.UrlCheck;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UrlCheckRepository extends BaseRepository {
+
+    public static void save(UrlCheck urlCheck) throws SQLException {
+        var sql = "INSERT INTO url_checks (url_id, status_code, h1, title, description, created_at) VALUES (?, ?, ?, ?, ?, ?)";
+        try (var conn = dataSource.getConnection();
+             var stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setLong(1, urlCheck.getUrlId());
+            stmt.setInt(2, urlCheck.getStatusCode());
+            stmt.setString(3, urlCheck.getH1());
+            stmt.setString(4, urlCheck.getTitle());
+            stmt.setString(5, urlCheck.getDescription());
+            stmt.setTimestamp(6, Timestamp.valueOf(urlCheck.getCreatedAt()));
+            stmt.executeUpdate();
+            var generatedKey = stmt.getGeneratedKeys();
+            if (generatedKey.next()) {
+                urlCheck.setId(generatedKey.getLong(1));
+            } else {
+                throw new SQLException("DB have not returned an id after saving an entity");
+            }
+        }
+    }
+
+    public static List<UrlCheck> getEntities(Long urlId) throws SQLException {
+        var sql = "SELECT * FROM url_checks WHERE url_id = ?";
+        try (var conn = dataSource.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+            stmt.setLong(1, urlId);
+            var resultSet = stmt.executeQuery();
+            List<UrlCheck> urlChecks = new ArrayList<>();
+            while (resultSet.next()) {
+
+                var statusCode = resultSet.getInt("status_code");
+                var h1 = resultSet.getString("h1");
+                var title = resultSet.getString("title");
+                var description = resultSet.getString("description");
+                var createdAt = resultSet.getTimestamp("created_at");
+                var id = resultSet.getLong("id");
+                var urlCheck = new UrlCheck(statusCode, h1, title, description, urlId, createdAt.toLocalDateTime());
+                urlCheck.setId(id);
+                urlChecks.add(urlCheck);
+            }
+            return urlChecks;
+        }
+    }
+}
