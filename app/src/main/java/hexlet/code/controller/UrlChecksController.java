c8a31510b6669e703c57a39310633a6d9641452a
DenisShakhurov <disa.sdf86@gmail.com>
2024-08-14 17:59:41 +0700

    refactoring UrlChecksController.java page.jte

--- app/src/main/java/hexlet/code/controller/UrlChecksController.java
+++ app/src/main/java/hexlet/code/controller/UrlChecksController.java
@@ -22,18 +22,19 @@ public class UrlChecksController {
                 .orElseThrow(() -> new NotFoundResponse("url не найден"));
         var urlName = url.getName();
         var jsonResponse = Unirest.get(urlName).asString();
+        var body = jsonResponse.getBody();
         var statusCode = jsonResponse.getStatus();
-        var title = findText(urlName, "title");
-        var h1 = findText(urlName, "h1");
-        var description = findText(urlName, "description");
+        var title = findText(body, "title");
+        var h1 = findText(body, "h1");
+        var description = findText(body, "description");
         var createdAt = LocalDateTime.now();
         var urlCheck = new UrlCheck(statusCode, h1, title, description, urlId, createdAt);
         UrlCheckRepository.save(urlCheck);
         ctx.redirect(NamedRoutes.urlPath(urlId));
     }
 
-    public static String findText(String url, String tag) throws IOException {
-        Document document = Jsoup.connect(url).userAgent("Chrome/81.0.4044.138").get();
+    public static String findText(String body, String tag) {
+        Document document = Jsoup.parse(body);
         switch (tag) {
             case "title" : return document.title();
             case "h1" : return document.selectFirst("h1") == null ? ""
