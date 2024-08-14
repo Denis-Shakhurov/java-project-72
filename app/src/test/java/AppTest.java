40aa0771bb1af39ca6f11ecdf3e5cb1ebbce9f90
DenisShakhurov <disa.sdf86@gmail.com>
2024-08-14 19:04:50 +0700

    refactoring AppTest.java

--- app/src/test/java/AppTest.java
+++ app/src/test/java/AppTest.java
@@ -5,8 +5,12 @@ import hexlet.code.repository.UrlRepository;
 import io.javalin.Javalin;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
-import org.junit.jupiter.api.*;
 import io.javalin.testtools.JavalinTest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
