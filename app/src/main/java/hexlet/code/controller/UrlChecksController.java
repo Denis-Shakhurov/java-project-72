84b802219d2691dc2791016fbbd4a7ec975853d4
DenisShakhurov <disa.sdf86@gmail.com>
2024-08-21 20:11:29 +0700

    update UrlCheksController

--- app/src/main/java/hexlet/code/controller/UrlChecksController.java
+++ app/src/main/java/hexlet/code/controller/UrlChecksController.java
@@ -7,29 +7,35 @@ import hexlet.code.util.NamedRoutes;
 import io.javalin.http.Context;
 import io.javalin.http.NotFoundResponse;
 import kong.unirest.core.Unirest;
+import kong.unirest.core.UnirestException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Objects;
 
 public class UrlChecksController {
 
-    public static void create(Context ctx) throws SQLException, IOException {
+    public static void create(Context ctx) throws SQLException {
+
         var urlId = ctx.pathParamAsClass("id", Long.class).get();
         var url = UrlRepository.find(urlId)
                 .orElseThrow(() -> new NotFoundResponse("url не найден"));
         var urlName = url.getName();
-        var jsonResponse = Unirest.get(urlName).asString();
-        var body = jsonResponse.getBody();
-        var statusCode = jsonResponse.getStatus();
-        var title = findText(body, "title");
-        var h1 = findText(body, "h1");
-        var description = findText(body, "description");
-        var urlCheck = new UrlCheck(statusCode, title, h1, description, urlId);
-        UrlCheckRepository.save(urlCheck);
-        ctx.redirect(NamedRoutes.urlPath(urlId));
+        try {
+            var jsonResponse = Unirest.get(urlName).asString();
+            var body = jsonResponse.getBody();
+            var statusCode = jsonResponse.getStatus();
+            var title = findText(body, "title");
+            var h1 = findText(body, "h1");
+            var description = findText(body, "description");
+            var urlCheck = new UrlCheck(statusCode, title, h1, description, urlId);
+            UrlCheckRepository.save(urlCheck);
+            ctx.redirect(NamedRoutes.urlPath(urlId));
+        } catch  (UnirestException e) {
+            ctx.sessionAttribute("flash", "Страница не доступна");
+            ctx.redirect(NamedRoutes.urlPath(urlId));
+        }
     }
 
     private static String findText(String body, String tag) {
